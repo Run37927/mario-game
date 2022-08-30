e7319d3a2319289bd35424238848c0ef20d552fe
Hairun Huang <hairunhuang@gmail.com>
2022-08-30 16:36:49 -0700

    platform collision detection

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ function animate(){
     };
 
     // rectangle collision detection
-    if(player.position.y + player.height <= platform.position.y && player.position.y + player.height+ player.velocity.y >= platform.position.y){
+    if(player.position.y + player.height <= platform.position.y && player.position.y + player.height+ player.velocity.y >= platform.position.y && player.position.x + player.width >= platform.position.x && player.position.x <= platform.position.x + platform.width){
         player.velocity.y = 0;
     }
 }
